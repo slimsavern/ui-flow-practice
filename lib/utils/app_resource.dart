a71c2bc2a95bf3cba35593e7e83d375b02e5bb7d
Elias Bansi <slimsavern@gmail.com>
2022-04-11 14:09:46 +0000

    first commit

---
+++ lib/utils/app_resource.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppResource {
+  static const String app_name = "Flutter Demo";
+  static const String app_version = "1.0.0";
+  static const String app_description = "A Flutter Demo";
+  static const String app_copyright = "Copyright © 2019";
+  static const String app_author = "Flutter Demo";
+  static const String app_email = "slimsavern@gmail.com";
+
+  static Color primaryColor = Colors.green;
+}
